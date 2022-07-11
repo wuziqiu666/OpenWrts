22fd99dd9859a5833b67bb2e21b7ce772c23e7c7
founSword <wuziqiu666@outlook.com>
2022-07-11 16:38:32 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 # Modify default system settings
 
-# 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
-
-# Hello World
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# 修改默认IP为192.168.2.1
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate 
 
 # luci-theme-infinityfreedom
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
-
-# passwall
-echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
-echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
 
 # 替换默认主题
 rm -rf package/lean/luci-theme-argon 
